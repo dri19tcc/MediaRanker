690d7ebc31e2e5c0d905da99b540e9c413c01db9
Adriana <dri19tcc@gmail.com>
2016-07-14 15:07:50 -0700

    Took out jade files, using ejs

--- app.js
+++ app.js
@@ -8,8 +8,11 @@ var routes = require('./routes/index');
 var app = express();
 
 // view engine setup
+//not using jade
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -18,9 +21,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
